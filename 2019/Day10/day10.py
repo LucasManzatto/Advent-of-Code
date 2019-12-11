f8a400bc6bfea537374ca322b68e03d6268b9ec9
Lucas Manzatto <lucas_peres21@hotmail.com>
2019-12-10 23:19:37 -0300

    implementing day 10 part 1

--- 2019/Day10/day10.py
+++ 2019/Day10/day10.py
@@ -6,7 +6,7 @@ for y, line in enumerate(space):
         if obj == '#':
             asteroids.append((x, y))
 
-test = asteroids[0]
+test = asteroids[6]
 distances = {}
 print(test)
 for asteroid in asteroids:
@@ -17,7 +17,7 @@ for asteroid in asteroids:
 sorted_distances = {k: v for k, v in sorted(
     distances.items(), key=lambda item: item[1])}
 # {k: v for k, v in sorted_distances.items() if v == distance}
-print(sorted_distances)
+print(asteroids)
 can_not_see = []
 for ast, distance in sorted_distances.items():
     if ast not in can_not_see:
@@ -26,18 +26,34 @@ for ast, distance in sorted_distances.items():
             ) if k[0] == ast[0] and k[1] > ast[1]}
             if len(after_ast) > 0:
                 can_not_see.append(after_ast)
-        if test[1] == ast[1]:
+        elif test[1] == ast[1]:
             after_ast = {k: v for k, v in sorted_distances.items(
             ) if k[1] == ast[1] and k[0] > ast[0]}
             if len(after_ast) > 0:
                 can_not_see.append(after_ast)
-        if abs(test[0] - ast[0]) == abs(test[1] - ast[1]):
+        elif abs(test[0] - ast[0]) == abs(test[1] - ast[1]):
             x, y = ast
-            while x < 10:
-                x += 1 if test[0] + ast[0] > 0 else -1
+            count = 0
+            while count < 10:
+                count += 1
+                x += 1 if test[0] < ast[0] else -1
                 y += 1
+                adjacent = sorted_distances.get((x, y), False)
+                if adjacent:
+                    can_not_see.append((x, y))
+        else:
+            x, y = ast
+            dist_to_x = abs(test[0] - ast[0])
+            dist_to_y = abs(test[1] - ast[1])
+            print(f'Found rectangle:{ast} , dist to x:{dist_to_x},dist to y:{dist_to_y}')
+            count = 0
+            while count < 10:
+                count+=1
+                x += dist_to_x if test[0] < ast[0] else -dist_to_x
+                y += dist_to_y if test[1] < ast[1] else -dist_to_y
                 adjacent = sorted_distances.get((x, y), False)
                 if adjacent:
                     can_not_see.append((x, y))
 
 print(can_not_see)
+print(len(sorted_distances) - len(can_not_see))
