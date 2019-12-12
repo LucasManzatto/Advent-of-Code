edb9ccc0ef9ec170d779dad5fdb2412497b8456f
Lucas Manzatto <lucasmanzatto21@gmail.com>
2019-12-12 14:49:34 -0300

    day10 part 2

--- 2019/Day10/day10.py
+++ 2019/Day10/day10.py
@@ -1,45 +1,32 @@
 from collections import namedtuple
-import itertools
-import math
-space = open('input_test2.txt').read().splitlines()
+from math import *
 Coordinates = namedtuple('Coordinates', 'x y')
+
+space = open('input.txt').read().splitlines()
 asteroids = []
 for y, line in enumerate(space):
     for x, obj in enumerate(line):
         if obj == '#':
             asteroids.append(Coordinates(x, y))
 
-def get_angles(asteroids, ast_to_test, radians=True):
-    angles = {}
+
+def distance(a, b):
+    return abs(a.x - b.x)+abs(a.y - b.y)
+
+
+def get_angle(a, b):
+    return degrees(atan2((a.x - b.x), (a.y - b.y)))
+
+
+def compute_lines(base):
+    lines = {}
     for ast in asteroids:
-        angle = math.atan2(ast.x - ast_to_test.x,ast.y -  ast_to_test.y)
-        angle = angle if radians else math.degrees(angle)
-        angles[angle]= ast
-    if len(angles) == 326:
-        print(ast)
-    return angles
+        angle = get_angle(ast, base)
+        lines[angle] = [ast]
+    return base, lines
 
 
-best_asteroid = max([len(get_angles(asteroids, asteroid))
-                     for asteroid in asteroids])
-print(best_asteroid)
-
-def calculate_distance(ast1, ast2):
-    return (math.sqrt(math.pow(ast1.x - ast2.x, 2) + math.pow(ast1.y - ast2.y, 2)), ast2)
-
-
-
-test = Coordinates(11,13)
-asteroids = list(filter(lambda x: x != test,asteroids))
-angles = get_angles(asteroids, test, radians=False)
-print(len(angles))
-vaporized_asteroids = [angles[i] for i in sorted(angles.keys(),reverse=True)]
-print(vaporized_asteroids[199])
-# first_nine = list(itertools.islice(sorted_angles.values(), 9))
-asteroids = [x for x in asteroids if x not in vaporized_asteroids]
-
-# angles = get_angles(asteroids, test, radians=False)
-# sorted_angles = {i: angles[i] for i in sorted(angles.keys(),reverse=True)}
-# last = list(itertools.islice(sorted_angles.values(), 2))
-
-# print(last)
+lines_of_sight = map(compute_lines, asteroids)
+(best_ast,line_of_sight) = max(lines_of_sight, key=lambda x: len(x[1]))
+last_erased = [line_of_sight[angle][0] for angle in sorted(compute_lines(best_ast)[1].keys(),reverse=True)][199]
+print(len(line_of_sight), best_ast, last_erased)
